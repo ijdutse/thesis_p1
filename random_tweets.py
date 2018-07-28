0931e67e9da9e2cf46a3616fc41d3e45d605bb42
Isa Dutse <ijdutse@yahoo.com>
2018-07-28 16:32:51 +0100

    Second changes to include main function

--- random_tweets.py
+++ random_tweets.py
@@ -1,15 +1,26 @@
 import numpy as np
 import pandas as pd
 import random
-
-df = pd.read_csv('short_tweets.csv')
-
-print(len(df.Text))
-# this has not been staged ..... or cimmitted!
 from difflib import SequenceMatcher as sm
+
+# Specify the file path to use:
+file_path =  'home/ijdutse/phase2/separation_degrees/short_tweets.csv'
+
+eets.csv')
+
+def main():
+	list_random()
+	
+
+
 def list_random(ran):
+	with open('home/ijdutse/phase2/separation_degrees/short_tweets.csv', 'r') as f:
+		tweet = f.readlines()
     random.shuffle(ran)
     x = ran[0]
     y = ran[1]
     sim = sm(None,x, y)
     return round(sim.ratio()*100, 2), x, y
+
+if __name__=='__main__':
+	main()

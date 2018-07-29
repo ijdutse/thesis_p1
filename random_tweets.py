53b64de708f3f4e851ee6a6a3efedaba8d71dfb0
Isa Dutse <ijdutse@yahoo.com>
2018-07-29 15:47:14 +0100

    All changes/updates take effect from this point ....

--- random_tweets.py
+++ random_tweets.py
@@ -1,44 +1,32 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
+# PACKAGES IMPORT
 import numpy as np
 import pandas as pd
 import random
 import os
 from difflib import SequenceMatcher as sm
 
-# Specify the file path to use:
-file_path ='/home/ijdutse/phase2/separation_degrees/text.txt'
+# THE FILE PATH
+file_path ='/home/ijdutse/thesis_p1/short_tweets.txt'
 
 
-
+# THE MAIN FUNCTION
 def main():
     with open(file_path,'r') as f:
         text = f.readlines()
         get_random_tweets_sim(text)
-        #print ('ALL DONE!')
-        #random.shuffle(tweets)
-        #x = tweets[0]
-        #y = tweets[1]
-        #sim = sm(None, x, y)
-        #print('IM YET TO BE EXCEUTED!')
-        #print(round(sim.ratio() * 100, 2))
         # return round(sim.ratio() * 100, 2)
 
-
+# FUNCTION TO PICK RANDOM TWEETS AND COMPUTE THE MATCHING SCORE ... MATCHING SCORE > 50 ARE LIKELY TO BE SAME/DUPLICATE TWEETS, HENCE DISCARDED
 def get_random_tweets_sim(tweets):
     random.shuffle(tweets)
     x = tweets[0]
     y = tweets[1]
     sim = sm(None,x, y)
-    #print('IM YET TO BE EXCEUTED!')
     print (round(sim.ratio()*100,2), sim.ratio())
     #return round(sim.ratio()*100,2), sim.ratio()
 
-
+# INSTANTITATE THE MAIN FUNCTION
 if __name__=='__main__':
-    #with open(file_path,'r') as f:
-     #   text = f.readlines()
-      #  get_random_tweets_sim(text)
-       # print (len(text))
-
 	main()

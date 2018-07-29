df7b239cdeb777b9137217293628a7dc775048aa
Isa Dutse <ijdutse@yahoo.com>
2018-07-29 15:18:50 +0100

    All changes/updates will take effect from this point ....

--- random_tweets.py
+++ random_tweets.py
@@ -1,26 +1,44 @@
+#!/usr/bin/env python
+
 import numpy as np
 import pandas as pd
 import random
+import os
 from difflib import SequenceMatcher as sm
 
 # Specify the file path to use:
-file_path =  'home/ijdutse/phase2/separation_degrees/short_tweets.csv'
+file_path ='/home/ijdutse/phase2/separation_degrees/text.txt'
 
-eets.csv')
+
 
 def main():
-	list_random()
-	
+    with open(file_path,'r') as f:
+        text = f.readlines()
+        get_random_tweets_sim(text)
+        #print ('ALL DONE!')
+        #random.shuffle(tweets)
+        #x = tweets[0]
+        #y = tweets[1]
+        #sim = sm(None, x, y)
+        #print('IM YET TO BE EXCEUTED!')
+        #print(round(sim.ratio() * 100, 2))
+        # return round(sim.ratio() * 100, 2)
 
 
-def list_random(ran):
-	with open('home/ijdutse/phase2/separation_degrees/short_tweets.csv', 'r') as f:
-		tweet = f.readlines()
-    random.shuffle(ran)
-    x = ran[0]
-    y = ran[1]
+def get_random_tweets_sim(tweets):
+    random.shuffle(tweets)
+    x = tweets[0]
+    y = tweets[1]
     sim = sm(None,x, y)
-    return round(sim.ratio()*100, 2), x, y
+    #print('IM YET TO BE EXCEUTED!')
+    print (round(sim.ratio()*100,2), sim.ratio())
+    #return round(sim.ratio()*100,2), sim.ratio()
+
 
 if __name__=='__main__':
+    #with open(file_path,'r') as f:
+     #   text = f.readlines()
+      #  get_random_tweets_sim(text)
+       # print (len(text))
+
 	main()

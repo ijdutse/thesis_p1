fd6c5a5d94118e924c23cea6f10618efcf62fc8e
Isa Dutse <ijdutse@yahoo.com>
2018-08-02 12:48:20 +0100

    From now own updates will be assined numeric values .. this is UPDATE1

--- random_tweets.py
+++ random_tweets.py
@@ -19,24 +19,36 @@ file_path ='/home/ijdutse/tweets_separation_degrees/short_tweets.txt'
 # THE MAIN FUNCTION ... crux of the activity!
 def main():
     with open(file_path,'r') as f:
-        text = f.readlines()
+        tweets = f.readlines()
 
-        # 1. return tweets devoid of duplicates or near duplicates:
-        tweet1, tweet2, m_score, relevant_tweets = get_tweets_matching(text) #print('The two random tweets have a matching score of: {}'.format(m_score))
+        #1: return tweets devoid of duplicates or near duplicates:
+        tweet1, tweet2, m_score, relevant_tweets = get_tweets_matching(tweets) 
+        #sanity check ==> print(tweet1, tweet2,m_score, relevant_tweets, sep='\n') or _, _, _, relevant_tweets = get_tweets_matching(tweets)
         
-        # 2. convert the relevant tweets to tfidf matrix for ease of similarity computation:
-        tweets_tfidf = get_tfidf_matrix(relevant_tweets) 
-        #print(tweets_tfidf)
-        #print(tweets_tfidf.toarray())
+        #2: convert relevant tweets (from #1) to tfidf matrix for ease of similarity computation:
+        tweets_tfidf_matrix = get_tfidf_matrix(relevant_tweets) 
+        #sanity check ==> print(tweets_tfidf_matrix, tweets_tfidf_matrix.toarray(), tweets_tfidf_matrix.toarray()[0:1], relevant_tweets, sep='\n')
         
-        #3. print the similarities matrix of the first 5 tweets with the rest:
-        #print(cosine_similarity(tweets_tfidf.toarray()[0:5], tweets_tfidf))
+        # using get_words_count_matrix:
+        tweets_count_matrix = get_words_count_matrix(relevant_tweets)
+        #sanity check ==> print(tweets_count_matrix, tweets_tfidf_matrix[0:1], relevant_tweets, sep='\n')
         
-        # map respective similarity score to corresponding tweet .... 
+        #3: print the similarities matrix of the first 5 tweets with the rest using both get_tfidf_matrix and get_words_count_matrix functions:
+        #tweet_similarity = np.round(cosine_similarity(tweets_tfidf_matrix.toarray()[0:5], tweets_tfidf_matrix), 4)
+        #tweet_similarity = np.round(cosine_similarity(tweets_count_matrix[0:5], tweets_count_matrix), 4)
+        #sanity check ==> print(tweet_similarity, relevant_tweets, sep='\n')
+        # compute the angle between similar tweets:
+		#co_sim = #sim_score of any document in the matrix
+		#radian_angle = math.acos(co_sim)
+		#degree_angle = math.degrees(radian_angle)
+		#print(degree_angle)
+
+  
+        #4: A MAPPER FUNCTION .... to map respective similarity score to corresponding tweet .... 
         #x = score_tweet_mapper()
 
 
-# FUNCTION TO PICK RANDOM TWEETS AND COMPUTE THE MATCHING SCORE ... matching score > 50 are likely to be same/duplicate ... hence discarded
+# A FUNCTION TO PICK RANDOM TWEETS AND COMPUTE THE MATCHING SCORE ... matching score > 50 are likely to be same/duplicate ... hence discarded
 def get_tweets_matching(tweets):
     scores = []
     relevant_tweets = []
@@ -55,46 +67,32 @@ def get_tweets_matching(tweets):
     		relevant_tweets.append(tweet1)
     		relevant_tweets.append(tweet2)
     	tracker +=1
-    return tweet1, tweet2, matching_score, relevant_tweets
-    # [score for score in scores if score>30], keep
+    return tweet1, tweet2, matching_score, relevant_tweets # [score for score in scores if score>30], keep
 
 
 # TRANSFORM TWEETS TO USING TFIDF SCHEME ... numeric form for ease of computation ... 
 
+# using tfidf:
 def get_tfidf_matrix(tweets):
-	# using tfidf:
 	vectorizer = TfidfVectorizer()
-	tfidf_matrix = vectorizer.fit_transform(tweets) #print(tfidf_matrix.shape), print(vectorizer.get_feature_names())
-	
-	# using countvectorizer:
-	#count_vect = CountVectorizer()
-	#words_count = count_vect.fit_transform(tweets) #concise_X = squareform(pdist(bag_of_words.toarray(), 'cosine'))
-	#concise_words_count = squareform(pdist(words_count.toarray()))
+	tweets_tfidf_matrix = vectorizer.fit_transform(tweets)
+	return tweets_tfidf_matrix
 
-	return tfidf_matrix, #words_count, concise_words_count #return tfidf_matrix, tfidf_matrix.toarray().flatten()[:5]
+# using Countvectrorizer and sqaureform/pdist from scipy::
+def get_words_count_matrix(tweets):
+	count_vectorizer = CountVectorizer()
+	words_count = count_vectorizer.fit_transform(tweets)
+	#use the scipy squareform to return concise version of the words from pairwise distance(pdist) space ... pdist takes numerous distance metrics e.g. 'euclidean, cosine'
+	dense_words_matrix = squareform(pdist(words_count.toarray(), 'cosine'))
+	return dense_words_matrix 
 
 
-# COMPUTE THE SIMILARITY BETWEEN TWO RANDOM TWEETS USING THE COSINE MEASURE
-#def get_tweets_sim(x, y):
-#	return lambda document: round()cosine_similarity(x,y)
-	# IF USING transform_tweets function:
-	#cosine_similarity(tfidf_matrix[0:1], tfidf_matrix)
-	#print(cosine_similarity(concise_X[0:2], concise_X))
-	#print(cosine_similarity(concise_X[:], concise_X))
-	# using the TFIDF Matrix to compute similarities:
-	#sim1 = cosine_similarity(tfidf_matrix[0:5], tfidf_matrix)
-	# using concise_words_count to compute similaritiy:
-
-	#sim2 = cosine_similarity(concise_X[:,:], concise_X)
-	
-	# COMPUTE THE ANGLE BETWEEN THE TWO TWEETS:
-	#co_sim = #sim_score of any document in the matrix
-	#radian_angle = math.acos(co_sim)
-	#degree_angle = math.degrees(radian_angle)
-	#print(degree_angle)
+# A MAPPER FUNCTION .... to map respective similarity score to corresponding tweet .... 
+        #x = score_tweet_mapper()
 
 #def score_tweet_mapper():
 #	x = map (lambda tweets_doc: round(cosine_similarity(tweets_tfidf, compare_tweet),3)
+
 
 
 
